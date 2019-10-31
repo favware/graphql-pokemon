6d72ff3e6750b5ad4f0b483388bba3cbd6b7661d
Jeroen Claassens <support@favware.tech>
2019-11-01 00:01:30 +0100

    chore: setup stuff

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ const app = express();
 apolloServer.applyMiddleware({ app });
 
 app.get('/', (_req, res) => {
-  return res.send('test');
+  return res.send('Looks like you hit a non-existing endpoint!');
 });
 
 // If we're not in the Cloud Functions environment, spin up a Node server
-if (!process.env.FUNCTION_NAME) {
+if (!process.env.FIREBASE_CONFIG) {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
     console.log('server started on http://localhost:4000/graphql');

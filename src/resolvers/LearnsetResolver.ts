ee7476c3874cf332d21043c7455a47f8c40b2360
Jeroen Claassens <j.claassens@cgi.com>
2019-11-30 04:51:15 +0100

    feat(learnsetresolver): new query: getPokemonLearnsetByFuzzy
    With this query you can get the learnset for a given pokemon and moves with having a fuzzy search
    applied to both of these input values. Generation parameter is also still supported for this query.
    
    fixes #54

--- src/resolvers/LearnsetResolver.ts
+++ src/resolvers/LearnsetResolver.ts
@@ -2,13 +2,21 @@ import { Args, Query, Resolver } from 'type-graphql';
 import LearnsetEntry from '../structures/LearnsetEntry';
 import LearnsetService from '../services/LearnsetService';
 import LearnsetArgs from '../arguments/LearnsetArgs';
+import DexService from '../services/DexService';
+import MoveService from '../services/MoveService';
+import Util from '../utils/util';
+import LearnsetFuzzyArgs from '../arguments/LearnsetFuzzyArgs';
 
 @Resolver(LearnsetEntry)
 export default class LearnsetResolver {
   private learnsetService: LearnsetService;
+  private dexService: DexService;
+  private moveService: MoveService;
 
   constructor() {
     this.learnsetService = new LearnsetService();
+    this.dexService = new DexService();
+    this.moveService = new MoveService();
   }
 
   @Query(() => LearnsetEntry, {
@@ -32,5 +40,40 @@ export default class LearnsetResolver {
     }
 
     return entry;
+  }
+
+  @Query(() => LearnsetEntry, {
+    description: [
+      'Gets the learnset for a given Pokémon and move.',
+      'A fuzzy search is performed to find a matching Pokémon and move',
+      'Multiple moves are possible by putting them in an array: [move1, move2].',
+      'You can also apply a generation filter (only results for the given generation will be returned) with the generation argument'
+    ].join(''),
+  })
+  getPokemonLearnsetByFuzzy(@Args() { pokemon, moves, generation }: LearnsetFuzzyArgs) {
+    const pokemonEntry = this.dexService.findBySpecies(pokemon);
+
+    if (!pokemonEntry) {
+      const fuzzyEntry = this.dexService.getByFuzzy({ pokemon, skip: 0, take: 10 });
+      if (fuzzyEntry === undefined) {
+        throw new Error(`Failed to get data for Pokémon: ${pokemon}`);
+      }
+      pokemon = Util.toLowerSingleWordCase(fuzzyEntry[0].species);
+    }
+
+    for (const [ index, move ] of moves.entries()) {
+      const moveEntry = this.moveService.findByName(move);
+      if (!moveEntry) {
+        const fuzzyEntry = this.moveService.findByFuzzy({
+          move, skip: 0, take: 10, reverse: false,
+        }, { threshold: 0.5 });
+        if (fuzzyEntry === undefined || !fuzzyEntry.length) {
+          throw new Error(`Failed to get data for move: ${move}`);
+        }
+        moves[index] = Util.toLowerSingleWordCase(fuzzyEntry[0].name);
+      }
+    }
+
+    return this.getPokemonLearnset({ pokemon, moves, generation });
   }
 }

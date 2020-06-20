c2768ff1ab76715c3e4eb54d2e3c638ede7c7bdc
Jeroen Claassens <support@favware.tech>
2020-06-20 14:46:27 +0200

    build(scripts): cleanup yarnAfterPull script

--- scripts/yarnAfterPull.ts
+++ scripts/yarnAfterPull.ts
@@ -4,24 +4,14 @@ import { kConsole } from './auto/utils';
 
 async function main() {
   const rootDir = resolve(__dirname, '..');
+  const { stdout, stderr } = await exec('yarn', { cwd: rootDir });
 
-  const { stdout } = await exec('git show --name-only', { cwd: rootDir });
-  const lastCommitFiles = stdout.split('\n').slice(6);
-
-  const hasLockfileInCommit = lastCommitFiles.some((commitFile) => commitFile.toLowerCase() === 'yarn.lock');
-
-  if (!hasLockfileInCommit) {
-    kConsole.log('Yarn lockfile was updated, running Yarn for you');
-
-    const { stdout, stderr } = await exec('yarn', { cwd: rootDir });
-
-    if (stderr) {
-      kConsole.error('An error occurred while running "yarn install"!');
-      kConsole.error(stderr);
-    } else {
-      kConsole.log(stdout);
-      kConsole.log('Successfully ran yarn for you!');
-    }
+  if (stderr) {
+    kConsole.error('An error occurred while running "yarn install"!');
+    kConsole.error(stderr);
+  } else {
+    kConsole.log(stdout);
+    kConsole.log('Successfully ran yarn for you!');
   }
 }
 

e946e41a9e50026126169612b56155af569af1d5
Jeroen Claassens <support@favware.tech>
2020-07-01 17:15:49 +0200

    chore: cleanup

--- scripts/yarnAfterPull.ts
+++ scripts/yarnAfterPull.ts
@@ -1,17 +1,17 @@
 import { exec } from '@klasa/utils';
 import { resolve } from 'path';
-import { kConsole } from './auto/utils';
+import Util from '../src/utils/util';
 
 async function main() {
   const rootDir = resolve(__dirname, '..');
   const { stdout, stderr } = await exec('yarn', { cwd: rootDir });
 
   if (stderr) {
-    kConsole.error('An error occurred while running "yarn install"!');
-    kConsole.error(stderr);
+    Util.kConsole.error('An error occurred while running "yarn install"!');
+    Util.kConsole.error(stderr);
   } else {
-    kConsole.log(stdout);
-    kConsole.log('Successfully ran yarn for you!');
+    Util.kConsole.log(stdout);
+    Util.kConsole.log('Successfully ran yarn for you!');
   }
 }
 

0cf1088926bf2f24c2e6f082a36be5a236b278e6
Jeroen Claassens <support@favware.tech>
2020-07-12 01:57:20 +0200

    build: fix learnset updater script

--- scripts/auto/pokemonlearnsets.ts
+++ scripts/auto/pokemonlearnsets.ts
@@ -25,7 +25,7 @@ const TIMESTAMP = new Timestamp('YYYY-MM-DD[T]HH:mm:ssZ').display(TEN_DAYS_AGO);
 
 const autoUpdateLearnsets = async () => {
   const url = new URL('https://api.github.com/repos/smogon/pokemon-showdown/commits');
-  url.searchParams.append('path', 'data/formats-data.ts');
+  url.searchParams.append('path', 'data/learnsets.ts');
   url.searchParams.append('since', TIMESTAMP);
 
   const request = await fetch(url);

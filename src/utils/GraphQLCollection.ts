21bbabb948abcadcd1350989b3d35fc009452fcc
Jeroen Claassens <support@favware.tech>
2021-07-01 00:27:30 +0200

    chore: cleanup unused code

--- src/utils/GraphQLCollection.ts
+++ src/utils/GraphQLCollection.ts
@@ -1,9 +1,7 @@
 import Collection from '@discordjs/collection';
-import Util from '#utils/util';
 
 export default class GraphQLCollection<K extends string, V> extends Collection<K, V> {
   private cachedEnumObj!: Record<K, K> | null;
-  private cachedSimplifiedEnumObj!: Record<K, K> | null;
 
   public enumObject(): Record<K, K> {
     if (!this.cachedEnumObj) {
@@ -16,19 +14,5 @@ export default class GraphQLCollection<K extends string, V> extends Collection<K
     }
 
     return this.cachedEnumObj;
-  }
-
-  public simplifiedEnumObject(): Record<K, K> {
-    if (!this.cachedSimplifiedEnumObj) {
-      const res: Record<K, K> = {} as Record<K, K>;
-      for (let key of this.keys()) {
-        key = Util.toLowerSingleWordCase<K>(key);
-        res[key] = key;
-      }
-
-      this.cachedSimplifiedEnumObj = res;
-    }
-
-    return this.cachedSimplifiedEnumObj;
   }
 }

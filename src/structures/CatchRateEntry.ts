37a95bc6d1fc2b201abe65014b4fbf9597adf289
nandhagk <nandhagopi@gmail.com>
2021-09-04 21:52:24 +0530

    fix(structures): change CatchRateEntry.base from string to number

--- src/structures/CatchRateEntry.ts
+++ src/structures/CatchRateEntry.ts
@@ -1,9 +1,9 @@
-import { Field, ObjectType } from 'type-graphql';
+import { Field, Int, ObjectType } from 'type-graphql';
 
 @ObjectType({ description: 'A Pokémon catch rate entry' })
 export default class CatchRateEntry {
-  @Field(() => String, { description: '' })
-  public base!: string;
+  @Field(() => Int, { description: '' })
+  public base!: number;
 
   @Field(() => String, { description: '' })
   public percentageWithOrdinaryPokeballAtFullHealth!: string;

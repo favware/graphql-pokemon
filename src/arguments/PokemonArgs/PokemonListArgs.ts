397abf1bb4a33d116a95195ed7ee8c7a3aff14fb
Jeroen Claassens <support@favware.tech>
2022-03-06 10:43:13 +0100

    fix: allow proper maximum amount for `getAllPokemonSpecies`

--- src/arguments/PokemonArgs/PokemonListArgs.ts
+++ src/arguments/PokemonArgs/PokemonListArgs.ts
@@ -6,11 +6,11 @@ import { ArgsType, Field, Int } from 'type-graphql';
 export class PokemonListArgs extends BaseFuzzyArgs {
   @Field(() => Int, {
     nullable: true,
-    defaultValue: 1271,
+    defaultValue: 1272,
     description: 'Return only this many results, starting from the offset'
   })
   @IsNumber()
   @Min(1)
-  @Max(1271)
-  public take = 1271;
+  @Max(1272)
+  public take = 1272;
 }

976facb5e734d9d2df292d2629532466a22266c8
Jeroen Claassens <support@favware.tech>
2022-05-13 18:43:39 +0200

    build: clean swc cache with clean script

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,12 +1,16 @@
 import { rm } from 'node:fs/promises';
 
-const generatedDir = new URL('../generated/', import.meta.url);
+const rootDir = new URL('../', import.meta.url);
+const generatedDir = new URL('generated/', rootDir);
+
+const swcCacheDir = new URL('.swc/', rootDir);
 const apiDir = new URL('api/', generatedDir);
 const tsDir = new URL('ts/', generatedDir);
 
 const options = { force: true, recursive: true };
 
 await Promise.all([
+  rm(swcCacheDir, options), //
   rm(apiDir, options), //
   rm(tsDir, options) //
 ]);

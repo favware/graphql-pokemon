62b7d689b67b6781883e1f0b04e0bff30e59ccaa
Jeroen Claassens <support@favware.tech>
2022-12-12 14:23:55 +0100

    chore: add flavor text updater script

--- scripts/data-gen-scripts/flavor-text-updater.mjs.mjs
+++ scripts/data-gen-scripts/flavor-text-updater.mjs.mjs
@@ -1,7 +1,7 @@
 import { fetch, FetchResultTypes } from '@sapphire/fetch';
 import { blue, bold, green, red, yellow } from 'colorette';
 import * as cheerio from 'https://cdn.skypack.dev/cheerio';
-import { readFile, writeFile } from 'node:fs/promises';
+import { readFile, writeFile, appendFile } from 'node:fs/promises';
 import { pokedex } from '../../api/lib/assets/pokedex.js';
 
 const bulbapediaBaseUrlPrefix = 'https://bulbapedia.bulbagarden.net/w/index.php?title=';
@@ -9,10 +9,15 @@ const bulbapediaBaseUrlPostfix = '_(Pok%C3%A9mon)&action=edit';
 const pathToFlavorTextFile = new URL('../../src/lib/assets/flavorText.json', import.meta.url);
 const failedPokemonTextFile = new URL('./failed-pokemon.json', import.meta.url);
 const skippedPokemonTextFile = new URL('./skipped-pokemon.json', import.meta.url);
+const logFile = new URL('./output.log', import.meta.url);
 const failedPokemon = [];
 const skippedPokemon = [];
 
 const flavorTexts = JSON.parse(await readFile(pathToFlavorTextFile));
+
+async function appendToLog(data, indent = true) {
+  await appendFile(logFile, `\n${indent ? '\t' : ''}${data}`, { encoding: 'utf-8' });
+}
 
 function parseSpeciesForBulbapedia(pokemonData) {
   if (pokemonData.specialBulbapediaUrl) {
@@ -48,26 +53,44 @@ for (const dexEntry of pokedex.values()) {
 }
 
 for (const pokemon of parsedPokemon) {
-  console.group(blue(`Parsing ${pokemon.species} (${pokemon.number}${pokemon.forme ?? ''})`));
+  const groupLogMessage = `Parsing ${pokemon.species} (${pokemon.number}${pokemon.forme ?? ''})`;
+  console.group(blue(groupLogMessage));
+  await appendToLog(groupLogMessage, false);
 
   if (pokemon.forme === undefined) {
-    console.log(yellow('Passed forme check'));
+    const formCheckMsg = 'Passed forme check';
+    console.log(yellow(formCheckMsg));
+    await appendToLog(formCheckMsg);
 
     const html = await fetch(pokemon.url, FetchResultTypes.Text);
-    console.log(yellow('Fetched data'));
+    const fetchedDataMsg = 'Fetched data';
+    console.log(yellow(fetchedDataMsg));
+    await appendToLog(fetchedDataMsg);
 
     const $ = cheerio.load(html);
-    console.log(yellow('Loaded text into cheerio'));
+    const cheerioLoadedMsg = 'Loaded text into cheerio';
+    console.log(yellow(cheerioLoadedMsg));
+    await appendToLog(cheerioLoadedMsg);
 
     const text = $('#wpTextbox1').text();
-    console.log(yellow('Loaded text element'));
+    const textElementLoadedMsg = 'Loaded text element';
+    console.log(yellow(textElementLoadedMsg));
+    await appendToLog(textElementLoadedMsg);
 
-    const svData = getTextContent(
+    const svDataScarletBased = getTextContent(
       text //
         ?.split('\n')
         ?.find((e) => e.startsWith('{{Dex/Entry2|v=Brilliant Diamond|v2=Shining Pearl'))
     );
-    console.log(yellow(`Retrieved SV Combined data, it is ${svData ? 'defined' : 'not defined'}`));
+    const svDataVioletBased = getTextContent(
+      text //
+        ?.split('\n')
+        ?.find((e) => e.startsWith('{{Dex/Entry2|v=Violet|v2=Scarlet'))
+    );
+    const svData = svDataScarletBased || svDataVioletBased;
+    const retrievedSvDataMsg = `Retrieved SV Combined data, it is ${svData ? 'defined' : 'not defined'}`;
+    console.log(yellow(retrievedSvDataMsg));
+    await appendToLog(retrievedSvDataMsg);
 
     if (svData) {
       flavorTexts[pokemon.number].push(
@@ -80,7 +103,9 @@ for (const pokemon of parsedPokemon) {
           flavor_text: svData
         }
       );
-      console.log(green('Stored new SV Combined entries in flavor texts'));
+      const storedSvMsg = 'Stored new SV Combined entries in flavor texts';
+      console.log(green(storedSvMsg));
+      await appendToLog(storedSvMsg);
     }
 
     const scarletData = getTextContent(
@@ -88,14 +113,18 @@ for (const pokemon of parsedPokemon) {
         ?.split('\n')
         ?.find((e) => e.startsWith('{{Dex/Entry1|v=Scarlet'))
     );
-    console.log(yellow(`Retrieved Scarlet data, it is ${scarletData ? 'defined' : 'not defined'}`));
+    const retrievedScarletMsg = `Retrieved Scarlet data, it is ${scarletData ? 'defined' : 'not defined'}`;
+    console.log(yellow(retrievedScarletMsg));
+    await appendToLog(retrievedScarletMsg);
 
     if (scarletData) {
       flavorTexts[pokemon.number].push({
         version_id: 'Scarlet',
         flavor_text: scarletData
       });
-      console.log(green('Stored new Scarlet Singular entry in flavor texts'));
+      const storedScarletMsg = 'Stored new Scarlet Singular entry in flavor texts';
+      console.log(green(storedScarletMsg));
+      await appendToLog(storedScarletMsg);
     }
 
     const violetData = getTextContent(
@@ -104,14 +133,18 @@ for (const pokemon of parsedPokemon) {
         ?.find((e) => e.startsWith('{{Dex/Entry1|v=Violet'))
     );
 
-    console.log(yellow(`Retrieved Violet data, it is ${violetData ? 'defined' : 'not defined'}`));
+    const retrievedVioletMsg = `Retrieved Violet data, it is ${violetData ? 'defined' : 'not defined'}`;
+    console.log(yellow(retrievedVioletMsg));
+    await appendToLog(retrievedVioletMsg);
 
     if (violetData) {
       flavorTexts[pokemon.number].push({
         version_id: 'Violet',
         flavor_text: violetData
       });
-      console.log(green('Stored new Violet Singular entry in flavor texts'));
+      const storedVioletMsg = 'Stored new Violet Singular entry in flavor texts';
+      console.log(green(storedVioletMsg));
+      await appendToLog(storedVioletMsg);
     }
 
     if (!violetData && !scarletData && !svData) {
@@ -119,7 +152,9 @@ for (const pokemon of parsedPokemon) {
         pokemon,
         text
       });
-      console.log(red('Did not store data for Pokemon'));
+      const didnotStoreDataMsg = 'Did not store data for Pokemon';
+      console.log(red(didnotStoreDataMsg));
+      await appendToLog(didnotStoreDataMsg);
     }
   } else {
     skippedPokemon.push({
@@ -130,9 +165,13 @@ for (const pokemon of parsedPokemon) {
   console.groupEnd();
 }
 
-console.log(bold(green('Done fetching and storing all data, writing to disk')));
+const doneFetchingMsg = 'Done fetching and storing all data, writing to disk';
+console.log(bold(green(doneFetchingMsg)));
+await appendToLog(doneFetchingMsg, false);
 await writeFile(pathToFlavorTextFile, JSON.stringify(flavorTexts, null, 4), { encoding: 'utf-8' });
-console.log(bold(green('Done writing to disk')));
+const doneWritingToDiskMsg = 'Done writing to disk';
+console.log(bold(green(doneWritingToDiskMsg)));
+await appendToLog(doneWritingToDiskMsg, false);
 
 await writeFile(failedPokemonTextFile, JSON.stringify(failedPokemon, null, 4), { encoding: 'utf-8' });
 await writeFile(skippedPokemonTextFile, JSON.stringify(skippedPokemon, null, 4), { encoding: 'utf-8' });

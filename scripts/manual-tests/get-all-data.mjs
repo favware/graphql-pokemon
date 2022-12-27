107e18a803cd0c17e12b13dc92c7532d166b5094
Jeroen Claassens <support@favware.tech>
2022-12-27 13:16:19 +0100

    chore: split get all data into gens

--- scripts/manual-tests/get-all-data.mjs
+++ scripts/manual-tests/get-all-data.mjs
@@ -4,16 +4,8 @@ import { promisify } from 'node:util';
 
 const pipeline = promisify(syncPipeline);
 
-const outputFile = new URL('./output.json', import.meta.url);
-
-const res = await fetch('http://localhost:4000', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    query: `
-      fragment GenerationalPokemonLearnsetFragment on GenerationalPokemonLearnset {
+const query = `
+ fragment GenerationalPokemonLearnsetFragment on GenerationalPokemonLearnset {
         generation3 {
           ...PokemonLearnsetFragment
         }
@@ -264,15 +256,198 @@ const res = await fetch('http://localhost:4000', {
         }
       }
 
-      query GetPokemon {
-        getAllPokemon {
+      query GetPokemon($offset: Int, $take: Int) {
+        getAllPokemon(offset: $offset, take: $take) {
           ...FullData
         }
       }
-    `
+    `;
+
+const headers = {
+  'Content-Type': 'application/json'
+};
+
+const url = new URL('http://localhost:4000');
+
+const resPokestar = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 0,
+      take: 17
+    }
   })
 });
 
-const readable = Readable.from(res.body, { encoding: 'utf-8' });
-const writable = createWriteStream(outputFile);
-await pipeline(readable, writable);
+const resCap = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 17,
+      take: 70
+    }
+  })
+});
+
+const resGen1Part1 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 87,
+      take: 116
+    }
+  })
+});
+
+const resGen1Part2 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 203,
+      take: 117
+    }
+  })
+});
+
+const resGen2 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 320,
+      take: 113
+    }
+  })
+});
+
+const resGen3 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 433,
+      take: 165
+    }
+  })
+});
+
+const resGen4 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 598,
+      take: 142
+    }
+  })
+});
+
+const resGen5 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 740,
+      take: 184
+    }
+  })
+});
+
+const resGen6 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 924,
+      take: 92
+    }
+  })
+});
+
+const resGen7 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 1016,
+      take: 129
+    }
+  })
+});
+
+const resGen8 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 1145,
+      take: 133
+    }
+  })
+});
+
+const resGen9 = await fetch(url, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    query,
+    variables: {
+      offset: 1278,
+      take: 111
+    }
+  })
+});
+
+const readablePokestar = Readable.from(resPokestar.body, { encoding: 'utf-8' });
+const readableCap = Readable.from(resCap.body, { encoding: 'utf-8' });
+const readableGen1Part1 = Readable.from(resGen1Part1.body, { encoding: 'utf-8' });
+const readableGen1Part2 = Readable.from(resGen1Part2.body, { encoding: 'utf-8' });
+const readableGen2 = Readable.from(resGen2.body, { encoding: 'utf-8' });
+const readableGen3 = Readable.from(resGen3.body, { encoding: 'utf-8' });
+const readableGen4 = Readable.from(resGen4.body, { encoding: 'utf-8' });
+const readableGen5 = Readable.from(resGen5.body, { encoding: 'utf-8' });
+const readableGen6 = Readable.from(resGen6.body, { encoding: 'utf-8' });
+const readableGen7 = Readable.from(resGen7.body, { encoding: 'utf-8' });
+const readableGen8 = Readable.from(resGen8.body, { encoding: 'utf-8' });
+const readableGen9 = Readable.from(resGen9.body, { encoding: 'utf-8' });
+
+const writablePokestar = createWriteStream(new URL('./res-pokestar.json', import.meta.url));
+const writableCap = createWriteStream(new URL('./res-cap.json', import.meta.url));
+const writableGen1Part1 = createWriteStream(new URL('./res-gen1-part1.json', import.meta.url));
+const writableGen1Part2 = createWriteStream(new URL('./res-gen1-part2.json', import.meta.url));
+const writableGen2 = createWriteStream(new URL('./res-gen2.json', import.meta.url));
+const writableGen3 = createWriteStream(new URL('./res-gen3.json', import.meta.url));
+const writableGen4 = createWriteStream(new URL('./res-gen4.json', import.meta.url));
+const writableGen5 = createWriteStream(new URL('./res-gen5.json', import.meta.url));
+const writableGen6 = createWriteStream(new URL('./res-gen6.json', import.meta.url));
+const writableGen7 = createWriteStream(new URL('./res-gen7.json', import.meta.url));
+const writableGen8 = createWriteStream(new URL('./res-gen8.json', import.meta.url));
+const writableGen9 = createWriteStream(new URL('./res-gen9.json', import.meta.url));
+
+await pipeline(readablePokestar, writablePokestar);
+await pipeline(readableCap, writableCap);
+await pipeline(readableGen1Part1, writableGen1Part1);
+await pipeline(readableGen1Part2, writableGen1Part2);
+await pipeline(readableGen2, writableGen2);
+await pipeline(readableGen3, writableGen3);
+await pipeline(readableGen4, writableGen4);
+await pipeline(readableGen5, writableGen5);
+await pipeline(readableGen6, writableGen6);
+await pipeline(readableGen7, writableGen7);
+await pipeline(readableGen8, writableGen8);
+await pipeline(readableGen9, writableGen9);

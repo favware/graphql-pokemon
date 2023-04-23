266aa41ced3afa12d947aa6bb2bcca2b3cfb96ab
Jeroen Claassens <support@favware.tech>
2023-04-23 02:40:58 +0200

    fix: improve error message

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ REMOVAL NOTICE! This version of the API is no longer supported. For the latest v
 
 For more information please join the Discord server through https://discord.com/invite/sguypX8
 
-Running a Discord bot that uses this API? Please consider inviting the official Dragonite bot instead as it already uses the latest features and is actively maintained by the same developers who brought you graphql-pokemon.
-You can invite Dragonite through the URL: https://discord.com/oauth2/authorize?client_id=931264626614763530&permissions=412317240320&scope=applications.commands%20bot
+Hosting your own Discord bot that uses this API? Please consider inviting the official Dragonite#6692 bot instead, as it already uses the latest features and is actively maintained by the same developers who brought you graphql-pokemon.
+You can invite Dragonite#6692 through the URL: https://discord.com/oauth2/authorize?client_id=931264626614763530&permissions=412317240320&scope=applications.commands%20bot
 `;
 
 fastify.route({

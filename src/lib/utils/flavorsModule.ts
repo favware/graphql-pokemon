062a19fc2d3e0f6bbc81f015ad314668572e0116
Jeroen Claassens <support@favware.tech>
2023-11-27 00:33:34 +0100

    fix: make type for flavors module

--- src/lib/utils/flavorsModule.ts
+++ src/lib/utils/flavorsModule.ts
@@ -4,4 +4,5 @@ import { readFile } from 'node:fs/promises';
 const pathToFile = new URL('../assets/flavorText.json', import.meta.url);
 const file = await readFile(pathToFile, { encoding: 'utf-8' });
 
-export const flavorsModule: Record<string, PokemonTypes.FlavorText[]> = JSON.parse(file) as Record<string, PokemonTypes.FlavorText[]>;
+export const flavorsModule: FlavorsModule = JSON.parse(file) as FlavorsModule;
+export type FlavorsModule = Record<string, PokemonTypes.FlavorText[]>;

bdad8b5be8fdbb9d7b5143838937ca27726158b8
Jeroen Claassens <jeroen.claassens@rws.nl>
2024-01-08 09:25:12 +0100

    ci: wait for flaresolverr port manually

--- scripts/wait-for-port.sh
+++ scripts/wait-for-port.sh
@@ -4,8 +4,8 @@ PORT=${PORT:-4000}
 
 echo "Waiting for service to launch on port ${PORT}..."
 
-while ! nc -z localhost ${PORT}; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
+while ! nc -z localhost ${PORT}; do
+  sleep 0.1 # wait for 1/10th of a second before check again
 done
 
 echo "Service launched"

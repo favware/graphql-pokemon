1c7980ec53b1dc0c707364174cf0d7762560d739
Jeroen Claassens <support@favware.tech>
2024-03-02 18:33:11 +0100

    test: update test data

--- scripts/data-gen-scripts/scripted-updaters/update-test-files.ts
+++ scripts/data-gen-scripts/scripted-updaters/update-test-files.ts
@@ -2,21 +2,29 @@ import { fetch, FetchMethods, FetchResultTypes } from '@sapphire/fetch';
 import { writeFile } from 'node:fs/promises';
 import { getPokemonWithFullDataAndEvolutions } from '../../../tests/testUtils/queries/pokemon-all-data.js';
 import { userAgentHeader } from '../../utils.js';
+import prettier from 'prettier';
+import prettierConfig from '../../../.prettierrc.mjs';
 
 const fullDataResponsesFolder = new URL('../../../tests/testUtils/full-data-responses/', import.meta.url);
 
+const data = await Promise.all(['beldum', 'dragonair', 'eevee', 'rattataalola', 'salamence', 'syclar'].map((pokemon) => executeGraphQL(pokemon)));
 const [beldumData, dragonairData, eeveeData, rattataalolaData, salamenceData, syclarData] = await Promise.all(
-  ['beldum', 'dragonair', 'eevee', 'rattataalola', 'salamence', 'syclar'].map((pokemon) => executeGraphQL(pokemon))
+  data.map((response) => stringifyAndFormat(response))
 );
 
 await Promise.all([
-  writeFile(new URL('beldum.json', fullDataResponsesFolder), JSON.stringify(beldumData)),
-  writeFile(new URL('dragonair.json', fullDataResponsesFolder), JSON.stringify(dragonairData)),
-  writeFile(new URL('eevee.json', fullDataResponsesFolder), JSON.stringify(eeveeData)),
-  writeFile(new URL('rattata-alola.json', fullDataResponsesFolder), JSON.stringify(rattataalolaData)),
-  writeFile(new URL('salamence.json', fullDataResponsesFolder), JSON.stringify(salamenceData)),
-  writeFile(new URL('syclar.json', fullDataResponsesFolder), JSON.stringify(syclarData))
+  writeFile(new URL('beldum.json', fullDataResponsesFolder), beldumData),
+  writeFile(new URL('dragonair.json', fullDataResponsesFolder), dragonairData),
+  writeFile(new URL('eevee.json', fullDataResponsesFolder), eeveeData),
+  writeFile(new URL('rattata-alola.json', fullDataResponsesFolder), rattataalolaData),
+  writeFile(new URL('salamence.json', fullDataResponsesFolder), salamenceData),
+  writeFile(new URL('syclar.json', fullDataResponsesFolder), syclarData)
 ]);
+
+async function stringifyAndFormat(response: PokemonResponse): Promise<string> {
+  const formatted = prettier.format(JSON.stringify(response), { ...prettierConfig, parser: 'json' });
+  return formatted;
+}
 
 async function executeGraphQL(pokemon: string): Promise<PokemonResponse> {
   return fetch<PokemonResponse>(

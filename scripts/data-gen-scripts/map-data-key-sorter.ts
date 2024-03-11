0aa0233d1c9bb76f13710dc46d9420bc26f00528
Jeroen Claassens <support@favware.tech>
2024-03-11 16:47:47 +0100

    chore: fix abilities updater script
    [skip ci]

--- scripts/data-gen-scripts/map-data-key-sorter.ts
+++ scripts/data-gen-scripts/map-data-key-sorter.ts
@@ -1,38 +1,7 @@
 import { Collection } from '@discordjs/collection';
 import { moves as data } from '../../src/lib/assets/moves.js';
+import { sortObjectByKey } from '../utils.js';
 import { dataToClipboard } from './data-to-clipboard.js';
-
-export function sortObjectByKey<T extends object>(obj: T): T {
-  const keys: string[] = [];
-  const sortedObj: T = {} as T;
-
-  for (const key in obj) {
-    if (Reflect.has(obj, key)) {
-      keys.push(key);
-    }
-  }
-
-  keys.sort((a, b) => {
-    const lowercaseA = a.toLowerCase();
-    const lowercaseB = b.toLowerCase();
-
-    if (lowercaseA < lowercaseB) {
-      return -1;
-    }
-
-    if (lowercaseA > lowercaseB) {
-      return 1;
-    }
-
-    return 0;
-  });
-
-  for (const key of keys) {
-    sortedObj[key] = obj[key];
-  }
-
-  return sortedObj;
-}
 
 const dataSortedKeys = data.sort((_, __, c, d) => {
   if (c < d) {
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -39,6 +39,38 @@ export async function importFileFromWeb<T extends object>({ url, temporaryFileNa
   return tiersData;
 }
 
+export function sortObjectByKey<T extends object>(obj: T): T {
+  const keys: string[] = [];
+  const sortedObj: T = {} as T;
+
+  for (const key in obj) {
+    if (Reflect.has(obj, key)) {
+      keys.push(key);
+    }
+  }
+
+  keys.sort((a, b) => {
+    const lowercaseA = a.toLowerCase();
+    const lowercaseB = b.toLowerCase();
+
+    if (lowercaseA < lowercaseB) {
+      return -1;
+    }
+
+    if (lowercaseA > lowercaseB) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  for (const key of keys) {
+    sortedObj[key] = obj[key];
+  }
+
+  return sortedObj;
+}
+
 /**
  * Replaces occurrences of "Poke Ball" with "Poké Ball" and "Pokemon" with "Pokémon" in the input string.
  * @param input - The input string to be processed.

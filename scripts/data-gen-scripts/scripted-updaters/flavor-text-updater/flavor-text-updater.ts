ade6b0b4b8d025ddd28e227b48b95a5052278b16
Jeroen Claassens <support@favware.tech>
2024-04-01 15:09:01 +0200

    refactor: process only a quarter of the pokemon for flavor updates every day
    [skip publish]

--- scripts/data-gen-scripts/scripted-updaters/flavor-text-updater/flavor-text-updater.ts
+++ scripts/data-gen-scripts/scripted-updaters/flavor-text-updater/flavor-text-updater.ts
@@ -3,9 +3,8 @@ import { green } from 'colorette';
 import { writeFile } from 'node:fs/promises';
 import { format } from 'prettier';
 import prettierConfig from '../../../../.prettierrc.mjs';
-import { ensureLogfileExists, getBulbapediaReadyPokemon } from '../utils/bulbapedia-utils.js';
-// import { createFlaresolverrSession, destroyFlaresolverrSession } from '../utils/flaresolverr-session-management.js';
 import { rootDir } from '../../../utils.js';
+import { ensureLogfileExists, getBulbapediaReadyPokemon } from '../utils/bulbapedia-utils.js';
 import { logFile } from './constants.js';
 import { gameSorter } from './game-sorter.js';
 import { log } from './log-wrapper.js';
@@ -16,10 +15,27 @@ const failedPokemonTextFile = new URL('./failed-pokemon.json', import.meta.url);
 
 await ensureLogfileExists(logFile);
 
-for (const pokemon of getBulbapediaReadyPokemon()) {
-  // await createFlaresolverrSession();
+const pokemonToParse = getBulbapediaReadyPokemon();
+
+// Get the current day of the month
+const dayOfMonth = new Date().getDate();
+
+// Use modulo to get a number between 0 and 3
+const quarterToProcess = dayOfMonth % 4;
+
+// Calculate the size of each quarter
+const quarterSize = Math.ceil(pokemonToParse.length / 4);
+
+// Split the array into quarters
+const quarters = Array(4)
+  .fill(0)
+  .map((_, i) => pokemonToParse.slice(i * quarterSize, (i + 1) * quarterSize));
+
+// Select the quarter to process
+const pokemonToProcess = quarters[quarterToProcess];
+
+for (const pokemon of pokemonToProcess) {
   await parsePokemon(pokemon);
-  // await destroyFlaresolverrSession();
 }
 
 await log({ msg: "Done fetching and storing data in memory, sorting version_id's", color: green, isBold: true, isIndent: false });

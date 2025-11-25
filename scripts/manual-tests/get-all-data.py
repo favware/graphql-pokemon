f83b1819466aed8f1610ed10c16dd6f3a2579f6c
KunoichiZ <KunoichiZ@users.noreply.github.com>
2025-11-25 16:13:45 -0600

    fix: add all 26 new megas to total number of Pokemon

--- scripts/manual-tests/get-all-data.py
+++ scripts/manual-tests/get-all-data.py
@@ -277,7 +277,7 @@ query = '''
 # Send the request
 response = requests.post(
     'http://localhost:4000',
-    json={'query': query, 'variables': {'offset': 0, 'take': 1423}},
+    json={'query': query, 'variables': {'offset': 0, 'take': 1449}},
     headers={'Content-Type': 'application/json'}
 )
 
